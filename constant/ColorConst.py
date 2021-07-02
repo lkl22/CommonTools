1c7d6429ee31e70c8d2cee3dcba570801d371802
likunlun <sz.lkl@163.com>
2021-07-02 20:48:55 +0800

    add ColorConst.py

---
+++ constant/ColorConst.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# python 3.x
+# Filename: ColorConst.py
+# 定义一个ColorConst常量类定义QColor常量
+from PyQt5.QtGui import QColor
+
+
+class ColorConst:
+    Red = QColor(0xF44336)
+    Pink = QColor(0xE91E63)
+    Purple = QColor(0x9C27B0)
+    DeepPurple = QColor(0x673AB7)
+    Indigo = QColor(0x3F51B5)
+    Blue = QColor(0x2196F3)
+    LightBlue = QColor(0x03A9F4)
+    Cyan = QColor(0x00BCD4)
+    Teal = QColor(0x009688)
+    Green = QColor(0x4CAF50)
+    LightGreen = QColor(0x8BC34A)
+    Lime = QColor(0xCDDC39)
+    Yellow = QColor(0xFFEB3B)
+    Amber = QColor(0xFFC107)
+    Orange = QColor(0xFF9800)
+    DeepOrange = QColor(0xFF5722)
+    Brown = QColor(0x795548)
+    Grey = QColor(0x9E9E9E)
+    BlueGrey = QColor(0x607D8B)
+    Black = QColor(0x000000)
+    White = QColor(0xFFFFFF)
+
+    def __init__(self):
+        pass
