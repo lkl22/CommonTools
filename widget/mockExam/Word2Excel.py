218be00875af1b6588a29ffb09086461b817996d
likunlun <sz.lkl@163.com>
2022-08-28 09:48:25 +0800

    设置单元格样式

--- widget/mockExam/Word2Excel.py
+++ widget/mockExam/Word2Excel.py
@@ -6,6 +6,9 @@
 import re
 from docx import Document
 from collections import OrderedDict
+
+from openpyxl.styles import Alignment, PatternFill
+
 from util.OpenpyxlUtil import OpenpyxlUtil
 
 questionDescRule = re.compile("(\d+[．.])(.*)")
@@ -105,11 +108,33 @@ class Word2Excel:
         multiChoiceSheet = OpenpyxlUtil.addSheet(bk, "多选题", 1)
         for st in [oneChoiceSheet, multiChoiceSheet]:
             OpenpyxlUtil.writeSheet(st, 1, 1, "题干")
+            st.column_dimensions["A"].width = 48
+            st.column_dimensions["B"].width = 8
+            for ch in "CDEFGHIJKLMNO":
+                st.column_dimensions[ch].width = 16
             OpenpyxlUtil.writeSheet(st, 1, 2, "答案")
             OpenpyxlUtil.writeSheet(st, 1, 3, "备注")
             OpenpyxlUtil.writeSheet(st, 1, 4, "解析")
             for index, ch in enumerate("ABCDEFGHIJK"):
                 OpenpyxlUtil.writeSheet(st, 1, 5 + index, "选项" + ch)
+            for ch in "ABCDEFGHIJKLMNO":
+                st[ch + "1"].alignment = Alignment(
+                    horizontal='center',  # 水平对齐，可选general、left、center、right、fill、justify、centerContinuous、distributed
+                    vertical='center',  # 垂直对齐， 可选top、center、bottom、justify、distributed
+                    text_rotation=0,  # 字体旋转，0~180整数
+                    wrap_text=False,  # 是否自动换行
+                    shrink_to_fit=False,  # 是否缩小字体填充
+                    indent=0,  # 缩进值
+                )
+                st[ch + "1"].fill = PatternFill(
+                    patternType="solid",
+                    # 填充类型，可选none、solid、darkGray、mediumGray、lightGray、lightDown、lightGray、lightGrid
+                    fgColor="00ffff",  # 前景色，16进制rgb
+                    # bgColor="00ffff",  # 背景色，16进制rgb
+                    # fill_type=None,     # 填充类型
+                    # start_color=None,   # 前景色，16进制rgb
+                    # end_color=None      # 背景色，16进制rgb
+                )
 
         oneChoiceRow = 1
         multiChoiceRow = 1
