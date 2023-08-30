f7062146bf27b08fe027878f221e04cdb0b95167
likunlun <sz.lkl@163.com>
2023-08-30 21:49:43 +0800

    将指定格式的数据写入excel文件

--- util/excel/ExcelOperator.py
+++ util/excel/ExcelOperator.py
@@ -46,7 +46,45 @@ class ExcelOperator:
         pass
 
     @staticmethod
+    def saveToExcel(fp, sheetName, keys: [], data: [{}]):
+        if not keys or not data:
+            LogUtil.e('saveToExcel', 'no data add to excel')
+            return False
+        _, fileExt = os.path.splitext(fp)
+        operator: IExcelOperator
+        indexOffset = 0
+        if fileExt == '.xls':
+            operator = Excel2003Operator()
+        elif fileExt == '.xlsx':
+            operator = Excel2007Operator()
+            # xlsx的下标从1开始
+            indexOffset = 1
+        else:
+            LogUtil.e('saveToExcel', 'not support file ext: ', fileExt)
+            return None
+        keyMap = {}
+        for index, key in enumerate(keys):
+            keyMap[key] = index + indexOffset
+        bk = operator.createBook()
+        sheet = operator.addSheet(bk, sheetName)
+        for row, rowItem in enumerate(data):
+            for key, item in rowItem.items():
+                operator.writeSheet(sheet, row + indexOffset, keyMap[key], item['value'])
+        operator.saveBook(bk, fp)
+        return True
+
+    @staticmethod
     def __getFilterData(operator, sheet, rows, cols, indexOffset, extendInfo):
+        """
+        根据filter规则过滤需要的数据
+        :param operator: Excel操作类
+        :param sheet: sheet表数据
+        :param rows: 行数
+        :param cols: 列数
+        :param indexOffset: index偏移
+        :param extendInfo: 扩展信息
+        :return: 获取满足指定条件的数据
+        """
         titleIndex = DictUtil.get(extendInfo, KEY_TITLE_INDEX, 0)
         colKeys = DictUtil.get(extendInfo, KEY_COL_KEYS, None)
         primaryKey = DictUtil.get(extendInfo, KEY_PRIMARY_KEY, None)
@@ -83,6 +121,15 @@ class ExcelOperator:
 
     @staticmethod
     def __getAllData(operator, sheet, rows, cols, indexOffset):
+        """
+        获取所有数据
+        :param operator: Excel操作类
+        :param sheet: sheet表数据
+        :param rows: 行数
+        :param cols: 列数
+        :param indexOffset: index偏移
+        :return: 表格里所有数据
+        """
         res = []
         for row in range(indexOffset, rows + indexOffset):
             rowData = []
@@ -93,14 +140,17 @@ class ExcelOperator:
 
 
 if __name__ == "__main__":
-    LogUtil.d(ExcelOperator.getExcelData("/Users/likunlun/PycharmProjects/res/values/AndroidRes.xls", 'color',
-                                         {KEY_TITLE_INDEX: 0, KEY_COL_KEYS: ['key', 'normal', 'dark'],
-                                          KEY_PRIMARY_KEY: 'key',
-                                          KEY_FILTER_RULES: {'dark': ['', '#000000', '#43CDF9'],
-                                                             'normal': ['#6633B5E5', '#1CA9F2']}}))
-
-    LogUtil.d(
-        ExcelOperator.getExcelData("/Users/likunlun/PycharmProjects/CommonTools/resources/mockExam/题库模版.xlsx", '单选题',
-                                   {KEY_TITLE_INDEX: 0, KEY_COL_KEYS: ['题干', '答案', '选项A'],
-                                    KEY_PRIMARY_KEY: '题干',
-                                    KEY_FILTER_RULES: {'答案': ['A', 'C']}}))
+    # LogUtil.d(ExcelOperator.getExcelData("/Users/likunlun/PycharmProjects/res/values/AndroidRes.xls", 'color',
+    #                                      {KEY_TITLE_INDEX: 0, KEY_COL_KEYS: ['key', 'normal', 'dark'],
+    #                                       KEY_PRIMARY_KEY: 'key',
+    #                                       KEY_FILTER_RULES: {'dark': ['', '#000000', '#43CDF9'],
+    #                                                          'normal': ['#6633B5E5', '#1CA9F2']}}))
+    #
+    # LogUtil.d(
+    #     ExcelOperator.getExcelData("/Users/likunlun/PycharmProjects/CommonTools/resources/mockExam/题库模版.xlsx", '单选题',
+    #                                {KEY_TITLE_INDEX: 0, KEY_COL_KEYS: ['题干', '答案', '选项A'],
+    #                                 KEY_PRIMARY_KEY: '题干',
+    #                                 KEY_FILTER_RULES: {'答案': ['A', 'C']}}))
+    ExcelOperator.saveToExcel('./test.xlsx', '11', ['key', 'normal', 'dark'],
+                              [{'key': {'value': 'key'}, 'normal': {'value': 'normal'}, 'dark': {'value': 'dark'}},
+                               {'key': {'value': 'sdd'}, 'normal': {'value': '#ffddffff'}, 'dark': {'value': '#000dd000'}}, ])
