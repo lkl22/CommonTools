e573e0c55d06dbbf3d576c053b65146803cb7ec7
likunlun <sz.lkl@163.com>
2023-09-01 21:29:09 +0800

    LogAnalysisDialog 增加持久化保存

--- widget/analysis/LogAnalysisDialog.py
+++ widget/analysis/LogAnalysisDialog.py
@@ -8,6 +8,14 @@ from util.OperaIni import *
 
 TAG = "LogAnalysisDialog"
 
+DATETIME_FORMAT = 'yyyy-MM-dd HH:mm:ss'
+
+KEY_SECTION = 'LogAnalysis'
+KEY_LOG_FILE_PATH = 'logFilePath'
+KEY_LOG_FILE_REG = 'logFileReg'
+KEY_LOG_START_TIME = 'logStartTime'
+KEY_LOG_END_TIME = 'logEndTime'
+
 
 class LogAnalysisDialog(QtWidgets.QDialog):
     WINDOW_WIDTH = 350
@@ -26,6 +34,20 @@ class LogAnalysisDialog(QtWidgets.QDialog):
         self.setWindowTitle(WidgetUtil.translate(text="Log分析工具"))
 
         self.isDebug = isDebug
+        self.operaIni = OperaIni("../../resources/config/BaseConfig.ini" if isDebug else '')
+        self.logFilePath = self.operaIni.getValue(KEY_LOG_FILE_PATH, KEY_SECTION)
+        self.logFileReg = self.operaIni.getValue(KEY_LOG_FILE_REG, KEY_SECTION)
+
+        logEndTime = self.operaIni.getValue(KEY_LOG_END_TIME, KEY_SECTION)
+        if not logEndTime:
+            logEndTime = QDateTime.currentDateTime().toString(DATETIME_FORMAT)
+        self.logEndTime = QDateTime.fromString(logEndTime, DATETIME_FORMAT)
+
+        logStartTime = self.operaIni.getValue(KEY_LOG_START_TIME, KEY_SECTION)
+        if logStartTime:
+            self.logStartTime = QDateTime.fromString(logStartTime, DATETIME_FORMAT)
+        else:
+            self.logStartTime = self.logEndTime.addDays(-1)
 
         vLayout = WidgetUtil.createVBoxLayout(self, margins=QMargins(10, 10, 10, 10), spacing=10)
 
@@ -48,7 +70,7 @@ class LogAnalysisDialog(QtWidgets.QDialog):
         WidgetUtil.createPushButton(splitter, text="日志文件路径", minSize=QSize(120, const.HEIGHT),
                                     onClicked=self.getLogFilePath)
         self.logFilePathLineEdit = WidgetUtil.createLineEdit(splitter,
-                                                             text='/Users/likunlun/PycharmProjects/CommonTools/widget/analysis/test',
+                                                             text=self.logFilePath if self.logFilePath else '',
                                                              isEnable=False, sizePolicy=sizePolicy)
         vbox.addWidget(splitter)
 
@@ -63,9 +85,19 @@ class LogAnalysisDialog(QtWidgets.QDialog):
         splitter = WidgetUtil.createSplitter(box)
         WidgetUtil.createLabel(splitter, text="提取Log日期范围", minSize=QSize(120, const.HEIGHT))
         # 指定当前日期时间为控件的日期时间
-        self.startDateTimeEdit = WidgetUtil.createDateTimeEdit(splitter, sizePolicy=sizePolicy)
+        self.startDateTimeEdit = WidgetUtil.createDateTimeEdit(splitter,
+                                                               dateTime=self.logStartTime,
+                                                               maxDateTime=self.logEndTime,
+                                                               displayFormat=DATETIME_FORMAT,
+                                                               onDateTimeChanged=self.logStartTimeChanged,
+                                                               sizePolicy=sizePolicy)
         WidgetUtil.createLabel(splitter, text='-')
-        self.endDateTimeEdit = WidgetUtil.createDateTimeEdit(splitter, sizePolicy=sizePolicy)
+        self.endDateTimeEdit = WidgetUtil.createDateTimeEdit(splitter,
+                                                             dateTime=self.logEndTime,
+                                                             minDateTime=self.logStartTime,
+                                                             displayFormat=DATETIME_FORMAT,
+                                                             onDateTimeChanged=self.logEndTimeChanged,
+                                                             sizePolicy=sizePolicy)
         vbox.addWidget(splitter)
 
         hbox = WidgetUtil.createHBoxLayout()
@@ -82,12 +114,37 @@ class LogAnalysisDialog(QtWidgets.QDialog):
             self.logFilePathLineEdit.setText(fp)
         pass
 
-    def extractLog(self):
+    def logStartTimeChanged(self, dateTime: QDateTime):
+        self.logStartTime = dateTime
+        self.endDateTimeEdit.setMinimumDateTime(dateTime)
+        LogUtil.i('logStartTimeChanged', self.logStartTime.toString(DATETIME_FORMAT))
         pass
+
+    def logEndTimeChanged(self, dateTime: QDateTime):
+        self.logEndTime = dateTime
+        self.startDateTimeEdit.setMaximumDateTime(dateTime)
+        LogUtil.i('logEndTimeChanged', self.logEndTime.toString(DATETIME_FORMAT))
+        pass
+
+    def extractLog(self):
+        logFileDirPath = self.logFilePathLineEdit.text().strip()
+        if not logFileDirPath:
+            WidgetUtil.showErrorDialog(message="请选择日志文件所在目录")
+            return
+        fileReg = self.fileRegPathLineEdit.text().strip()
+        if not fileReg:
+            WidgetUtil.showErrorDialog(message="请输入匹配Log文件的正则表达式")
+            return
+        self.operaIni.addItem(KEY_SECTION, KEY_LOG_FILE_PATH, logFileDirPath)
+        self.operaIni.addItem(KEY_SECTION, KEY_LOG_FILE_REG, fileReg)
+        self.operaIni.addItem(KEY_SECTION, KEY_LOG_START_TIME, self.logStartTime.toString(DATETIME_FORMAT))
+        self.operaIni.addItem(KEY_SECTION, KEY_LOG_END_TIME, self.logEndTime.toString(DATETIME_FORMAT))
+        self.operaIni.saveIni()
+        pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = LogAnalysisDialog(isDebug=True)
     window.show()
     sys.exit(app.exec_())
-
