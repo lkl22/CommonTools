33043af6c1a0185a0b67210e82ab1ee80c8e695c
lkl <sz.lkl@163.com>
2023-12-28 23:10:53 +0800

    添加多行合并后数据处理脚本

---
+++ resources/efk/config/filebeat/js/handleSegmentData.js
@@ -0,0 +1,7 @@
+function process(event) {
+    var message = event.Get('message');
+    message = message.replace('Segment Data[0]', '');
+    // 必须完全匹配，不能直接使用.*，否则会替换多余的
+    message = message.replace(/\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}\.\d{3}\s+\d+[\s-]+\d+[\s/][?]?\s*\w{1}\s+[^:]+:\s+Segment Data\[\d+\]: /g, '');
+    event.Put('message', message);
+}
